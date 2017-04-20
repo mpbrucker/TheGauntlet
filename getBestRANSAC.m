aeee384c660b674f7acc17729ef3a95de3330200
Matt Brucker <mpbrucker@gmail.com>
2017-04-20 11:59:38 -0400

    Refactored code into two functions.

--- getBestRANSAC.m
+++ getBestRANSAC.m
@@ -7,34 +7,38 @@ function [line, inliers] = getBestRANSAC(points, n, d)
         pointRows = randRows(1:2); % Get two random rows
         testPoints = points(pointRows, :) % Pick the two random points
         
-        testLine = testPoints(2,:) - testPoints(1,:) % Get vector between the points
-        standPoints = points - testPoints(1,:); % Standardize points to pass through origin
-        clf;
-        plot(points(:,1), points(:,2), 'r*');
-        hold on;
-%         plot(standPoints(:,1), standPoints(:,2), 'b*');
-        perpVec = [-testLine(2) testLine(1)]; % Get perpendicular vector
-        standPerp = perpVec/norm(perpVec); % Standardize the perpendicular vector
+        insidePoints = getClosePoints(points, testPoints, d); % Get current inlier points
         
-%         quiver(testPoints(1,1), testPoints(1,2), testLine(1), testLine(2), 'Color', 'r');
-%         quiver(testPoints(1,1), testPoints(1,2), standPerp(1), standPerp(2), 'Color', 'g');
-        
-%         quiver(0, 0, testLine(1), testLine(2), 'Color', 'b');
-%         quiver(0, 0, standPerp(1), standPerp(2), 'Color', 'k');
-        grid on
-        daspect([1 1 1])
-        
-        projPoints = standPerp*standPoints' % Project points onto the perpendicular vector
-        validPoints = abs(projPoints) < d; % Find valid points
-        numValid = sum(validPoints); % Get number of valid points
+        numValid = size(insidePoints, 1); % Get number of valid points
         if (numValid > maxInliers)
             bestLine = testPoints; % Update the points for best line
             maxInliers = numValid; % Update the best
-            inliers = points(find(validPoints),:); % Filter valid points
+            inliers = insidePoints; % Filter valid points
         end 
     end
     line = bestLine
+    clf;
+    plot(points(:,1), points(:,2), 'r*');
+    hold on;
+%         plot(standPoints(:,1), standPoints(:,2), 'b*');
+%         quiver(testPoints(1,1), testPoints(1,2), testLine(1), testLine(2), 'Color', 'r');
+%         quiver(testPoints(1,1), testPoints(1,2), standPerp(1), standPerp(2), 'Color', 'g');
+
+%         quiver(0, 0, testLine(1), testLine(2), 'Color', 'b');
+%         quiver(0, 0, standPerp(1), standPerp(2), 'Color', 'k');
+    grid on
+    daspect([1 1 1])
     plot(inliers(:,1), inliers(:,2), 'go');
     plot(line(:,1), line(:,2), 'b');
+end
+
+function validPoints = getClosePoints(points, line, d)
+    testLine = line(2,:) - line(1,:) % Get vector between the points
+    standPoints = points - line(1,:); % Standardize points to pass through origin
+    perpVec = [-testLine(2) testLine(1)]; % Get perpendicular vector
+    standPerp = perpVec/norm(perpVec); % Standardize the perpendicular vector
+    projPoints = standPerp*standPoints'; % Project points onto the perpendicular vector
+    validIdx = abs(projPoints) < d; % Find valid points
+    validPoints = points(find(validIdx), :);
 end
 
