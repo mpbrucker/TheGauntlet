bf5abb0087c7985a3a9f3203195877f188f5a6da
Adam Selker <aselker@olin.edu>
2017-04-20 12:09:08 -0400

    Working on endpoint finding, and optimizing best-fitter

--- bestFit.m
+++ bestFit.m
@@ -1,13 +1,9 @@
-function [slope, y_int] = bestFit (points)
+function [dir, offset] = bestFit (points)
     
-    xMean = mean(points(:,1));                  %Mean-center points
-    yMean = mean(points(:,2));                  
-    adj = [points(:,1) - xMean, points(:,2) - yMean];
+    offset = mean(points);                  %Mean-center points                
+    adj = points - offset;
     
     [eigs, ~] = eig(adj' * adj);                %Find eigenvectors
     dir = eigs(:, size(eigs, 2));               %Take the most significant one
-    
-    slope = dir(2) / dir(1);
-    y_int = yMean - (xMean * slope);
-
+        
 end
