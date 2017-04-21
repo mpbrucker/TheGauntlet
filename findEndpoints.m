38a95e25fd4b983f5d4e7245858a8ae6fb89ed8d
aselker <aselker@openmailbox.org>
2017-04-21 14:56:31 -0400

    Mess with edge cases

--- findEndpoints.m
+++ findEndpoints.m
@@ -24,7 +24,7 @@ function [ end1, end2, insidePoints] = findEndpoints( points, sensitivity )
             start = i;
         end
         
-        if (abs(i-prev) >  threshhold) || (i == proj(length(proj))) %If the gap was above the threshhold or we're at the last point
+        if (abs(i-prev) >  threshhold) %If the gap was above the threshhold
             if (prev - start) > maxLen             %If this is the new longest
                 maxLen = (prev - start);        %Note that we should use the *previous* value because if it just failed then we've crossed the gap
                 end1 = start;
@@ -35,6 +35,13 @@ function [ end1, end2, insidePoints] = findEndpoints( points, sensitivity )
             
         prev = i;
         
+    end
+    
+                                        % Also run after the last point
+    if (prev - start) > maxLen          %If this is the new longest
+    	maxLen = (prev - start);        %Note that we should use the *previous* value because if it just failed then we've crossed the gap
+        end1 = start;
+        end2 = prev;
     end
     
     assert(~isnan(end1) && ~isnan(end2)); %Case with zero points, I think
