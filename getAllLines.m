d3c245807634f8cc94e664890f7b0de1f072c9d4
Matt Brucker <mpbrucker@gmail.com>
2017-04-21 14:59:20 -0400

    Tried to fix assertion issues.

--- getAllLines.m
+++ getAllLines.m
@@ -1,6 +1,6 @@
 function lines = getAllLines(r, theta, thresh)
     theta_clean = deg2rad(theta(r~=0)); % Get clean theta values
-    r_clean = deg2rad(r(r~=0)); % Get clean r values
+    r_clean = (r(r~=0)); % Get clean r values
     lines = []
     points = [r_clean.*cos(theta_clean) r_clean.*sin(theta_clean)] % Points represented in Cartesian coordinates
     i=1;
@@ -8,21 +8,20 @@ function lines = getAllLines(r, theta, thresh)
     plot(points(:,1), points(:,2), 'bo'); % Plot the original points
     hold on;
     while (size(points,1) > thresh) % While there are still points left
-        [line, inliers, outliers] = getBestRANSAC(points, 100, .0002); % Tweak this
+        [line, inliers, outliers] = getBestRANSAC(points, 500, .005); % Tweak this
+%         if(size(inliers,1)<=8)
+%             break;
+%         end
         [end1, end2, insidePoints] = findEndpoints(inliers, 1);
         
         
         lines(:, :, i) = [end1; end2];
-        plot(lines(:,1, end), lines(:,2,end), 'r');
+        plot(lines(:,1, end), lines(:,2,end), 'r', 'LineWidth', 3);
         i = i + 1;
 %         plot(insidePoints(:,1), insidePoints(:,2), 'g*');
-        points = getOutliers(points, insidePoints);
+        points = getOutliers(points, insidePoints); % Remove inlier points
 %         plot(points(:,1), points(:,2), 'g*');
 %         points = outliers;
     end
-%     for i=1:size(lines,3)
-%         plot(lines(:,1, i), lines(:,2,i), 'r');
-%         hold on;
-%     end
 
 end
