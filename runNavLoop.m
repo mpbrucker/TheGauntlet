ee5ff56ddb693fe4855f7dac9bcf24426bfc9b91
aselker <aselker@openmailbox.org>
2017-04-27 12:11:45 -0400

    Using the right scan technique now

--- runNavLoop.m
+++ runNavLoop.m
@@ -1,16 +1,23 @@
 tf = rostf;
 % wait for a bit so we can build up a few tf frames
 pause(2);
-subPointCloud = rossubscriber('/projected_stable_scan'); %Point-cloud subscriber
+subPoints = rossubscriber('/stable_scan'); %Point-cloud subscriber
 sub_bump = rossubscriber('/bump');          %Bump sensor to stop script
 pub = rospublisher('/raw_vel');             %Velocity publisher to move wheels
 msg = rosmessage(pub);                      %The message which will be sent to the wheels
 
 while true
     
-    pointCloud = receive(subPointCloud);
-    points = readXYZ(pointCloud);
-    points = points(:,1:2);
+    rMaxThreshhold = 5;
+
+    scan_message = recieve(subPoints);
+    r = scan_message.Ranges(1:end-1);
+    theta = [0:359]';
+    
+    r_keep = (r~=0) & (r<=rMaxThreshhold);
+    r_clean = r(r_keep);
+    theta_clean = deg2rad(theta(r_keep));
+    points = [r_clean.*cos(theta_clean) r_clean.*sin(theta_clean)]; % Points represented in Cartesian coordinates
     
     scatter(points(:,1), points(:,2), '.');
     
