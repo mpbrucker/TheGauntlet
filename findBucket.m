a4369b12e6cc3e2e4250946a7d924242f644da42
aselker <aselker@openmailbox.org>
2017-04-27 11:59:36 -0400

    Working on buckets

--- findBucket.m
+++ findBucket.m
@@ -5,8 +5,8 @@ function [ x, y, inliers, outliers ] = findBucket( points )
 
     distanceThreshhold = 0.1; %m - the distance to start a new point chunk
     targetRadius = 0.146; % m
-   	radiusThreshhold = 0.5; %...meters?
-    varianceThreshhold = 2; %Square meters I think
+   	radiusThreshhold = 0.1; %...meters?
+    varianceThreshhold = 0.1; %Square meters I think
     pointThreshhold = 5; %min point count
     
     end1 = NaN;
@@ -31,27 +31,29 @@ function [ x, y, inliers, outliers ] = findBucket( points )
             prev = index;
         end
         
+        plot(points(end1:prev,1), points(end1:prev, 2), 'g.');
+
         if (norm(points(index,:) - points(prev,:)) > distanceThreshhold) || index == length(points)
-           [thisX, thisY, thisRadius, thisVariance] = fitCircleLinear(points(end1:prev,1), points(end1:prev,2))
-           plot(points(end1:prev,1), points(end1:prev, 2), 'g*');
+           [thisX, thisY, thisRadius, thisVariance] = fitCircleLinear(points(end1:prev,1), points(end1:prev,2));
            if (abs(thisRadius - targetRadius) < radiusThreshhold) && (thisVariance < bestVariance) && (prev-end1) >= pointThreshhold
                bestX = thisX;
                bestY = thisY;
                bestVariance = thisVariance;
-               bestPointSet = [end1, prev];
+               bestPointSet = [end1 : prev];
            end
            end1 = index;
            prev = index;
         else
+            norm(points(index,:) - points(prev,:))
             prev = index;
         end
         
     end %end for
-        
+            
     
     if (isnan(bestVariance))
-        error('No bucket found!')
         points
+        error('No bucket found!')
     end
     
     x = bestX;
@@ -59,6 +61,9 @@ function [ x, y, inliers, outliers ] = findBucket( points )
     
     inliers = points(bestPointSet,:);
     outliers = points(~bestPointSet,:);
+    
+    plot(inliers(:,1), inliers(:,2), 'r*');    
+
 
 end
 
